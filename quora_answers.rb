bb84def937297bb5eb21c2b1db44745a21c7e3c1
Stormy Shippy <stormyshippy@gmail.com>
2011-03-10 17:58:14 -0800

    Added voter and commenter list csv output

--- quora_answers.rb
+++ quora_answers.rb
@@ -9,59 +9,73 @@ if not Dir.pwd == directory
   Dir.chdir directory
 end
 
-  doc = File.open("answers_#{ARGV[0]}.html")
-  page = Nokogiri::HTML(doc)
-  doc.close
-
-    def content(content_div)
-       content_div.css('.comments.answer_comments.hidden').remove
-       content_div.css('.answer_user').remove
-       content_div.css('.action_bar').remove
-       content_div
-    end
-
-    def comments(comment)
-       if comment.search('a[@class="view_comments supp "]').inner_text =~ /Add Comment/
-         0
-       else
-         comment.search('a[@class="view_comments supp "]').inner_text.split(' ')[0]
-       end
-    end
-
-    def voters(list)
-      array = Array.new
-      list.each_with_index do |x,i|
-        array[i] = x.attribute('href').value
-      end
-
-      array2 = Array.new
-      list.each_with_index do |x,i|
-        array2[i] = x.inner_text
-      end
-
-      ordered_list = Array.new
-      list.each_with_index do|x,i|
-        ordered_list[i] = [array2[i], array[i]]
-      end
-      ordered_list
-    end
-
+doc = File.open("answers_#{ARGV[0]}.html")
+pp "Now processing" + doc.inspect
+page = Nokogiri::HTML(doc)
+doc.close
 
 answerer_name = page.search('h1').inner_text
-pp answerer_name
 date = DateTime.now.to_s
 
-outfile = File.open("#{answerer_name}_answers_#{date}", 'ab')
+outfile = File.open("#{answerer_name}_answers_#{date}.csv", 'ab')
 CSV::Writer.generate(outfile, ',') do |csv|
  csv << [ "question", "question_url", "answerer", "answerer_url", "answerer_bio", "votes", "comments_num", "answer_date", "images",
-   "breaks", "italics", "blockquote", "list_item", "anchor", "underline", "codeblock", "latex", "answer_length", "answer", "answer_url", "voter_list" ]
+   "breaks", "italics", "blockquote", "list_item", "anchor", "underline", "codeblock", "latex", "answer_length", "answer", "answer_url", "voter_list", "commenter_list" ]
 end
+
+outfile_voters = File.open("#{answerer_name}_voters_#{date}.csv", 'ab')
+CSV::Writer.generate(outfile, ',') do |csv|
+ csv << [ "question", "voter_name", "voter_url" ]
+end
+
+outfile_commenters = File.open("#{answerer_name}_commenters_#{date}.csv", 'ab')
+CSV::Writer.generate(outfile, ',') do |csv|
+ csv << [ "question", "commenter_name", "commenter_url" ]
+end
+
+def content(content_div)
+   content_div.css('.comments.answer_comments.hidden').remove
+   content_div.css('.answer_user').remove
+   content_div.css('.action_bar').remove
+   content_div
+end
+
+def comments(comment)
+  if comment.search('a[@class="view_comments supp "]').inner_text =~ /Add Comment/
+   0
+  else
+    comment.search('a[@class="view_comments supp "]').inner_text.split(' ')[0]
+  end
+end
+
+def voters(list)
+  array = Array.new
+  list.each_with_index do |x,i|
+    array[i] = x.attribute('href').value
+  end
+
+  array2 = Array.new
+  list.each_with_index do |x,i|
+    array2[i] = x.inner_text
+  end
+
+  ordered_list = Array.new
+  list.each_with_index do|x,i|
+    ordered_list[i] = [array2[i], array[i]]
+  end
+  ordered_list
+end
+
 
 most_votes = []
 most_comments = []
 total_votes = 0
 total_comments = 0
 total_answers = 0
+total_images = 0
+total_quotes = 0
+total_links = 0
+voter_array = []
 
 page.css('div.e_col.w4_5').each_with_index do |answer, index|
   comment_urls = answer.search('p[@class="action_bar"]//a[@class="user"]').collect {|x| x.attribute('href').value}
@@ -95,6 +109,9 @@ page.css('div.e_col.w4_5').each_with_index do |answer, index|
   total_votes += answer_info['votes'].to_i
   total_comments += answer_info['comments_num'].to_i
   total_answers += 1
+  total_images += answer_info['images']
+  total_quotes += answer_info['blockquote']
+  total_links += answer_info['anchor']
 
   CSV::Writer.generate(outfile, ',') do |csv|
     csv << [ answer_info['question'], answer_info['question_url'], answer_info['answerer'], answer_info['answerer_url'], 
@@ -103,7 +120,21 @@ page.css('div.e_col.w4_5').each_with_index do |answer, index|
       answer_info['latex'], answer_info['answer_length'], answer_info['answer'], answer_info['answer_url'], answer_info['voter_list'],
       answer_info['commenter_list'] ]
   end
+
+  answer_info['voter_list'].each do |voter|
+    CSV::Writer.generate(outfile_voters, ',') do |csv|
+      csv << [ answer_info['question'], voter[0], voter[1] ]
+    end
+  end
+
+  answer_info['commenter_list'].each do |commenter|
+    CSV::Writer.generate(outfile_commenters, ',') do |csv|
+      csv << [ answer_info['question'], commenter[0], commenter[1] ]
+    end
+  end
+
 end
+
 
 b = Hash.new(0)
 c = Hash.new(0)
@@ -120,11 +151,15 @@ pp b.sort {|a,b| a[1]<=>b[1]}
 puts "\n"
 pp c.sort {|a,b| a[1]<=>b[1]}
 
-c.each do |k,v|
-  percent = v.quo(total_answers)*100.to_f
-  pp k + " has upvoted " + v.to_s + " answers and votes " + percent.to_s + " percent of the time."
-end
+#c.each do |k,v|
+  #percent = v.quo(total_answers)*100.to_f
+  #pp k + " has upvoted " + v.to_s + " answers and votes " + percent.to_s + " percent of the time."
+#end
 
-pp "Total answers: " + total_answers.to_s
-pp "Total votes: " + total_votes.to_s
-pp "Total comments: " + total_comments.to_s
+puts "Answer Summary Stats"
+puts "Total answers: " + total_answers.to_s
+puts "Total votes: " + total_votes.to_s
+puts "Total comments: " + total_comments.to_s
+puts "Total images: " + total_images.to_s
+puts "Total quotes: " + total_quotes.to_s
+puts "Total links: " + total_links.to_s
