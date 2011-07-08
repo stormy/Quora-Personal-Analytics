d2c8a6cf2efd7095613beb79719d766423bc4e05
Stormy Shippy <stormyshippy@gmail.com>
2011-07-08 02:39:09 -0700

    Voter class hash and eql? methods

--- lib/Voter.rb
+++ lib/Voter.rb
@@ -7,4 +7,22 @@ class Voter
     @fullname = voter.inner_text
     @url = voter.attribute('href').value
   end
+
+  def ==(other)
+    @url==other.url
+  rescue
+    false
+  end
+  
+  alias eql? ==
+
+  def hash
+    code = 17
+    code = 37*code + @url.hash
+    code = 37*code + @fullname.hash
+   #code = 37*code + @order.hash ##Voter could have a different order on different answer
+    code
+  end
+    
+
 end
