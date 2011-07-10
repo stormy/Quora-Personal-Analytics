90e421f9e7e16ac0031e714eea4af0b0822a1e06
Stormy Shippy <stormyshippy@gmail.com>
2011-07-10 03:45:40 -0700

    New Question stat methods

--- lib/QuoraUser.rb
+++ lib/QuoraUser.rb
@@ -31,6 +31,14 @@ class QuoraUser
     total_votes 
   end
 
+  def comments_total
+    total_comments = 0
+    answers.each do |x|
+      total_comments += x.total_comments
+    end
+    total_comments
+  end
+
   def voters_to_array
     all_voters = []
     answers.each do |x|
@@ -55,6 +63,51 @@ class QuoraUser
       puts voter[1].to_s + " votes from: " + voter[0].url + " (" + voter[0].fullname + ")"
     end
   end
+
+  def questions_total_answers
+    question_answers = 0
+    @questions.each do |x|
+      question_answers += x.answers_total
+    end
+    question_answers
+  end
+
+  def question_most_answered
+    @questions.max {|a,b| a.answers_total <=> b.answers_total}
+  end
+
+  def questions_total_followers
+    question_followers = 0
+    @questions.each do |x|
+      question_followers += x.followers_total
+    end
+    question_followers
+  end
+
+  def question_most_followed
+    @questions.max {|a,b| a.followers_total <=> b.followers_total}
+  end
+
+  def questions_not_answered
+    question_not_answered = 0
+    @questions.each do |x|
+      if x.answers_total == 0
+        question_not_answered += 1
+      end
+    end
+    question_not_answered
+  end
+
+  def questions_not_followed
+    question_not_followed = 0
+    @questions.each do |x|
+      if x.followers_total == 1
+        question_not_followed += 1
+      end
+    end
+    question_not_followed
+  end
+
 
 end
 
