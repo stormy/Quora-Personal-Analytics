8b64930f39bc241b77f7c03a3b1bf68179eeebff
Stormy Shippy <stormyshippy@gmail.com>
2011-07-10 03:44:59 -0700

    HTML changes required a few fixes

--- lib/Question.rb
+++ lib/Question.rb
@@ -1,13 +1,13 @@
 class Question
 
-  attr_reader :title, :url, :content, :answers_total, :topics_total, :topics
+  attr_reader :title, :url, :content, :answers_total, :topics_total, :topics, :followers_total
 
   def initialize(fragment)
     @title = fragment.css('.question_link').inner_text
     @url = fragment.css('.question_link').attribute('href').value
     @content = fragment.css('.feed_item_question_details').inner_text
-    @answers_total = fragment.css('.feed_item_question_info').inner_text.split(' ')[0]
-    @topics_total = fragment.css('.view_all_topics.hidden').inner_text.split(' ')[0]
-    @topics = QuestionTopic.new(fragment.css('.topic_name'))
+    @answers_total = fragment.css('.number_answers')[0].inner_text.split(' ')[0].to_i
+    @followers_total = fragment.css('.number_answers')[1].inner_text.split(' ')[0].to_i
+   #@topics = QuestionTopic.new(fragment.css('.topic_name')) #=> Quora HTML has changed, topics not shown now
   end
 end
